04867c1c3805a6422b7cd308d0639bf5a6c0585c
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-05-20 16:04:34 +0900

    prfix: prfix

--- sql/table.sql
+++ sql/table.sql
@@ -1,54 +1,44 @@
 CREATE TABLE `user` (
-  `id` integer PRIMARY KEY,
+  `id` serial PRIMARY KEY,
   `name` varchar(255) NOT NULL,
   `mail` varchar(255) UNIQUE NOT NULL,
-  `password` varchar(255) UNIQUE NOT NULL,
-  `profile_url` TEXT NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `profile_url` text NOT NULL,
   `created_at` timestamp default CURRENT_TIMESTAMP,
   `updated_at` timestamp default CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `article` (
-  `id` integer PRIMARY KEY,
+  `id` serial PRIMARY KEY,
   `title` varchar(255) NOT NULL,
-  `contents` TEXT NOT NULL,
+  `contents` text NOT NULL,
   `created_at` timestamp default CURRENT_TIMESTAMP,
-  `user_id` integer NOT NULL
+  `user_id` integer NOT NULL,
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 );
 
 CREATE TABLE `article_image` (
-  `id` integer PRIMARY KEY,
-  `url`TEXT NOT NULL,
+  `id` serial PRIMARY KEY,
+  `url` text NOT NULL,
   `created_at` timestamp default CURRENT_TIMESTAMP,
-  `article_id` integer NOT NULL
+  `article_id` integer NOT NULL,
+  CONSTRAINT `fk_article_id` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`)
 );
 
 CREATE TABLE `tag` (
-  `id` integer PRIMARY KEY,
+  `id` serial PRIMARY KEY,
   `name` varchar(255) NOT NULL,
   `created_at` timestamp default CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `article_tag` (
-  `id` integer PRIMARY KEY,
+  `id` serial PRIMARY KEY,
   `article_id` integer NOT NULL,
   `tag_id` integer NOT NULL,
-  `created_at` timestamp default CURRENT_TIMESTAMP
+  `created_at` timestamp default CURRENT_TIMESTAMP,
+  CONSTRAINT `fk_article_id` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`),
+  CONSTRAINT `fk_tag_id` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`)
 );
-
-ALTER TABLE `user_unsubscribed` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-
-ALTER TABLE `article` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-
-ALTER TABLE `article_deleted` ADD FOREIGN KEY (`article_id`) REFERENCES `article` (`id`);
-
-ALTER TABLE `article_image` ADD FOREIGN KEY (`article_id`) REFERENCES `article` (`id`);
-
-ALTER TABLE `article_tag` ADD FOREIGN KEY (`article_id`) REFERENCES `article` (`id`);
-
-ALTER TABLE `article_tag` ADD FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`);
-
-
 
 CREATE OR REPLACE FUNCTION update_updated_at_column()
 RETURNS TRIGGER AS $$
