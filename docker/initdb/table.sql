1fa4cfd719c6f009c1c5d0fbf7b3956bb4b33417
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-05-23 09:12:23 +0900

    fix テーブル定義の仕方をfix

--- docker/initdb/table.sql
+++ docker/initdb/table.sql
@@ -1,43 +1,44 @@
-CREATE TABLE `user` (
-  `id` serial PRIMARY KEY,
-  `name` varchar(255) NOT NULL,
-  `mail` varchar(255) UNIQUE NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `profile_url` text NOT NULL,
-  `created_at` timestamp default CURRENT_TIMESTAMP,
-  `updated_at` timestamp default CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS "user" (
+    id serial PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    mail varchar(255) UNIQUE NOT NULL,
+    password varchar(255) NOT NULL,
+    profile_url text NOT NULL,
+    created_at timestamp default CURRENT_TIMESTAMP,
+    updated_at timestamp default CURRENT_TIMESTAMP
+    );
+
+
+CREATE TABLE IF NOT EXISTS "article" (
+  id serial PRIMARY KEY,
+  title varchar(255) NOT NULL,
+  contents text NOT NULL,
+  created_at timestamp default CURRENT_TIMESTAMP,
+  user_id integer NOT NULL,
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES "user" (id)
 );
 
-CREATE TABLE `article` (
-  `id` serial PRIMARY KEY,
-  `title` varchar(255) NOT NULL,
-  `contents` text NOT NULL,
-  `created_at` timestamp default CURRENT_TIMESTAMP,
-  `user_id` integer NOT NULL,
-  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+CREATE TABLE IF NOT EXISTS "article_image" (
+  id serial PRIMARY KEY,
+  url text NOT NULL,
+  created_at timestamp default CURRENT_TIMESTAMP,
+  article_id integer NOT NULL,
+  CONSTRAINT fk_article_id FOREIGN KEY (article_id) REFERENCES article (id)
 );
 
-CREATE TABLE `article_image` (
-  `id` serial PRIMARY KEY,
-  `url` text NOT NULL,
-  `created_at` timestamp default CURRENT_TIMESTAMP,
-  `article_id` integer NOT NULL,
-  CONSTRAINT `fk_article_id` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`)
+CREATE TABLE IF NOT EXISTS "tag" (
+  id serial PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  created_at timestamp default CURRENT_TIMESTAMP
 );
 
-CREATE TABLE `tag` (
-  `id` serial PRIMARY KEY,
-  `name` varchar(255) NOT NULL,
-  `created_at` timestamp default CURRENT_TIMESTAMP
-);
-
-CREATE TABLE `article_tag` (
-  `id` serial PRIMARY KEY,
-  `article_id` integer NOT NULL,
-  `tag_id` integer NOT NULL,
-  `created_at` timestamp default CURRENT_TIMESTAMP,
-  CONSTRAINT `fk_article_id` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`),
-  CONSTRAINT `fk_tag_id` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`)
+CREATE TABLE IF NOT EXISTS "article_tag" (
+  id serial PRIMARY KEY,
+  article_id integer NOT NULL,
+  tag_id integer NOT NULL,
+  created_at timestamp default CURRENT_TIMESTAMP,
+  CONSTRAINT fk_article_id FOREIGN KEY (article_id) REFERENCES article (id),
+  CONSTRAINT fk_tag_id FOREIGN KEY (tag_id) REFERENCES tag (id)
 );
 
 CREATE OR REPLACE FUNCTION update_updated_at_column()
