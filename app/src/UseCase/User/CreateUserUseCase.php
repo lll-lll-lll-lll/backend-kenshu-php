e4959f42b7f69f0621f90daa456b999b5ee796df
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-05-24 12:10:16 +0900

    [feat] usecase実装

---
+++ app/src/UseCase/User/CreateUserUseCase.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace App\UseCase\User;
+
+use App\Repository\CreateUserRepository;
+use App\Request\CreateUserRequest;
+use Exception;
+use PDO;
+use PDOException;
+
+class CreateUserUseCase implements ICreateUserUseCase
+{
+    private PDO $pdo;
+
+    private CreateUserRepository $createUserRepository;
+
+    public function __construct(PDO $pdo, CreateUserRepository $createUserRepository)
+    {
+        $this->pdo = $pdo;
+        $this->createUserRepository = $createUserRepository;
+    }
+
+    public function execute(CreateUserRequest $req): void
+    {
+        $this->pdo->beginTransaction();
+        try {
+            $this->createUserRepository->execute($this->pdo, $req->user_name, $req->mail, $req->hash_password, $req->profile_url);
+            $this->pdo->commit();
+        } catch (Exception $e) {
+            $this->pdo->rollBack();
+            error_log('CreateUserUseCase/' . $e->getMessage());
+            throw new PDOException($e->getMessage());
+        }
+    }
+}
