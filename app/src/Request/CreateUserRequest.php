11d0f7bd41da25a147979dd9228fd03b830e6601
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-05-24 12:10:28 +0900

    [feat] request実装

---
+++ app/src/Request/CreateUserRequest.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Request;
+
+use InvalidArgumentException;
+
+class CreateUserRequest
+{
+    public string $user_name;
+    public string $mail;
+    public string $hash_password;
+    // プロフィール画像は任意なので空文字で初期化
+    public string $profile_url = '';
+
+    public function __construct(string $user_name, string $mail, string $password, string $profile_url = '')
+    {
+        $this->validateEmail($mail);
+        $this->validatePassword($password);
+        $this->validateUserName($user_name);
+
+        if (!empty($profile_url)) {
+            if (!filter_var($profile_url, FILTER_VALIDATE_URL)) {
+                throw new InvalidArgumentException('Invalid profile url');
+            }
+        }
+        $this->hash_password = password_hash($password, PASSWORD_DEFAULT);
+        $this->user_name = $user_name;
+        $this->mail = $mail;
+        $this->profile_url = $profile_url;
+    }
+
+
+    private function validateEmail(string $email): void
+    {
+        if (empty($email)) {
+            throw new InvalidArgumentException('Email is not empty');
+        }
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException('Invalid email');
+        }
+    }
+
+    private function validatePassword(string $password): void
+    {
+        if (empty($password)) {
+            throw new InvalidArgumentException('Password is not empty');
+        }
+        if (strlen($password) < 8) {
+            throw new InvalidArgumentException('Password is too short');
+        }
+    }
+
+    private function validateUserName(string $user_name): void
+    {
+        if (empty($user_name)) {
+            throw new InvalidArgumentException('User name is not empty');
+        }
+        if (strlen($user_name) > 255) {
+            throw new InvalidArgumentException('User name is too long');
+        }
+    }
+}
