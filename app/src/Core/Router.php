687bbc56898bb7fe1688640df52ebe8ef6139dd9
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-05-28 13:35:36 +0900

    [feat] path parameter取得用Router編集

--- app/src/Core/Router.php
+++ app/src/Core/Router.php
@@ -19,11 +19,15 @@ class Router
     public function dispatch(string $requestUri, string $requestMethod): void
     {
         foreach ($this->routes as $route) {
-            if ($route['route'] === $requestUri && $route['method'] === $requestMethod) {
-                call_user_func($route['callback']);
+            $pattern = preg_replace('/\{([a-zA-Z0-9_]+)}/', '(?P<$1>[a-zA-Z0-9_-]+)', $route['route']);
+            $pattern = '#^' . $pattern . '$#';
+            if (preg_match($pattern, $requestUri, $matches) && $route['method'] === $requestMethod) {
+                $params = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY);
+                call_user_func_array($route['callback'], $params);
                 return;
             }
         }
         http_response_code(404);
+        echo 'Not Found';
     }
 }
