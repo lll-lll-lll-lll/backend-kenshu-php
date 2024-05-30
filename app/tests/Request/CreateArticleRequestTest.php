14b76488d9cdb8cbfd7343d4e14ece77c61b6f67
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-05-30 16:02:25 +0900

    [fix] テスト追加。リクエストが正しい場合と、画像URLが正しくない場合と、URLは正しいが拡張子が指定のもの以外の場合

---
+++ app/tests/Request/CreateArticleRequestTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Request;
+
+use App\Request\CreateArticleRequest;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class CreateArticleRequestTest extends TestCase
+{
+    public function testCreateArticleRequest(): void
+    {
+        $title = 'title';
+        $contents = 'contents';
+        $thumbnail_image_url = 'https://example.com/image.jpg';
+        $user_id = 1;
+
+        $request = new CreateArticleRequest($title, $contents, $thumbnail_image_url, $user_id);
+
+        $this->assertSame($title, $request->title);
+        $this->assertSame($contents, $request->contents);
+        $this->assertSame($thumbnail_image_url, $request->thumbnail_image_url);
+        $this->assertSame($user_id, $request->user_id);
+    }
+
+    public function testCreateInvalidUrl(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid URL');
+
+        $title = 'title';
+        $contents = 'contents';
+        $thumbnail_image_url = 'invalid_url';
+        $user_id = 1;
+
+        new CreateArticleRequest($title, $contents, $thumbnail_image_url, $user_id);
+    }
+
+    public function testNotValidExtension(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid file extension: pdf');
+
+        $title = 'title';
+        $contents = 'contents';
+        $thumbnail_image_url = 'https://example.com/image.pdf';
+        $user_id = 1;
+
+        new CreateArticleRequest($title, $contents, $thumbnail_image_url, $user_id);
+    }
+}
