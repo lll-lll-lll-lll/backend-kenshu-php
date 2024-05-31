979d36fffb0b14faca1b5843193b1392384967b2
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-05-31 14:32:01 +0900

    [fix]　ファイル移動

--- app/src/UseCase/CreateArticleUseCase.php
+++ app/src/UseCase/CreateArticleUseCase.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace App\UseCase;
 
+use App\Repository\CreateArticleImageRepository;
 use App\Repository\CreateArticleRepository;
+use App\Repository\CreateArticleTagRepository;
+use App\Repository\GetTagRepository;
 use App\Request\CreateArticleRequest;
 use Exception;
 use PDO;
@@ -11,12 +14,23 @@ use PDO;
 class CreateArticleUseCase
 {
     private PDO $pdo;
-    private CreateArticleRepository $createRepository;
+    private CreateArticleRepository $createArticleRepository;
+    private CreateArticleTagRepository $createArticleTagRepository;
+    private CreateArticleImageRepository $createArticleImageRepository;
+    private GetTagRepository $getTagRepository;
 
-    public function __construct(PDO $pdo, CreateArticleRepository $createRepository)
+    public function __construct(
+        PDO                          $pdo,
+        CreateArticleRepository      $createRepository,
+        CreateArticleTagRepository   $createArticleTagRepository,
+        CreateArticleImageRepository $createArticleImageRepository,
+        GetTagRepository             $getTagRepository)
     {
         $this->pdo = $pdo;
-        $this->createRepository = $createRepository;
+        $this->createArticleRepository = $createRepository;
+        $this->createArticleTagRepository = $createArticleTagRepository;
+        $this->createArticleImageRepository = $createArticleImageRepository;
+        $this->getTagRepository = $getTagRepository;
     }
 
     /**
@@ -27,15 +41,16 @@ class CreateArticleUseCase
         try {
             $this->pdo->beginTransaction();
             try {
-                $articleId = $this->createRepository->executeInsertArticle($this->pdo, $req->title, $req->contents, $req->user_id);
-                $this->createRepository->executeInsertArticleImage($this->pdo, $req->thumbnail_image_url, $articleId);
-                $tagId = $this->createRepository->executeInsertTag($this->pdo, $req->tag_name);
-                $this->createRepository->executeInsertArticleTag($this->pdo, $articleId, $tagId);
+                $articleId = $this->createArticleRepository->execute($this->pdo, $req->title, $req->contents, $req->user_id);
+                $this->createArticleImageRepository->execute($this->pdo, $req->thumbnail_image_url, $articleId);
+                $tag_id = $this->getTagRepository->execute($this->pdo, $req->tag_id);
+                $this->createArticleTagRepository->execute($this->pdo, $articleId, $tag_id);
             } catch (Exception $e) {
                 throw new Exception($e->getMessage());
             }
             $this->pdo->commit();
         } catch (Exception $e) {
+            echo $e->getMessage();
             $this->pdo->rollBack();
             throw new Exception($e->getMessage());
         }
