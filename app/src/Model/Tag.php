ce4377475d58eadbe4ac18f9160d196ba024619d
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-05-31 15:59:46 +0900

    [feat] tag取得 Model実装

---
+++ app/src/Model/Tag.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model;
+
+class Tag
+{
+    public int $id;
+    public string $name;
+
+    public function __construct(int $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+}
