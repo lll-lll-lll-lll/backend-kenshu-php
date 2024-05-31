9325688e0707fe4c30d76dc78476dbf6868a3fde
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-05-31 15:58:48 +0900

    [feat] tag取得Handler

---
+++ app/src/Handler/GetTagListHandler.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Handler;
+
+use App\Model\Tag;
+use App\UseCase\GetTagListUseCase;
+use Exception;
+use PDO;
+
+class GetTagListHandler
+{
+    public GetTagListUseCase $getTagListUseCase;
+    public PDO $pdo;
+
+    public function __construct(PDO $pdo, GetTagListUseCase $getTagListUseCase)
+    {
+        $this->pdo = $pdo;
+        $this->getTagListUseCase = $getTagListUseCase;
+    }
+
+    /**
+     * @return array<Tag>
+     * @throws Exception
+     */
+    public function execute(): array
+    {
+        try {
+            return $this->getTagListUseCase->execute();
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+
+}
