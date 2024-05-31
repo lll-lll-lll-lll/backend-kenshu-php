b6a6839471992e8abe883425eb996c916b711550
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-05-31 14:31:44 +0900

    [fix] 初期化に追加

--- app/src/Main.php
+++ app/src/Main.php
@@ -9,10 +9,13 @@ use App\Handler\CreateArticleHandler;
 use App\Handler\CreateUserHandler;
 use App\Handler\GetArticleHandler;
 use App\Handler\GetArticleListHandler;
+use App\Repository\CreateArticleImageRepository;
 use App\Repository\CreateArticleRepository;
+use App\Repository\CreateArticleTagRepository;
 use App\Repository\CreateUserRepository;
 use App\Repository\GetArticleListRepository;
 use App\Repository\GetArticleRepository;
+use App\Repository\GetTagRepository;
 use App\UseCase\CreateArticleUseCase;
 use App\UseCase\GetArticleListUseCase;
 use App\UseCase\GetArticleUseCase;
@@ -34,7 +37,14 @@ class Main
         $this->router = new Router();
         $config = require 'Config.php';
         $this->pdo = Database::getConnection($config);
-        $this->articleCreateHandler = new CreateArticleHandler(new CreateArticleUseCase($this->pdo, new CreateArticleRepository()));
+        $this->articleCreateHandler = new CreateArticleHandler(
+            new CreateArticleUseCase(
+                $this->pdo,
+                new CreateArticleRepository(),
+                new CreateArticleTagRepository(),
+                new CreateArticleImageRepository(),
+                new GetTagRepository())
+        );
         $this->articleHandler = new GetArticleHandler(new GetArticleUseCase($this->pdo, new GetArticleRepository()));
         $this->articleListHandler = new GetArticleListHandler(new GetArticleListUseCase($this->pdo, new GetArticleListRepository()));
         $this->userCreateHandler = new CreateUserHandler(new CreateUserUseCase($this->pdo, new CreateUserRepository()));
