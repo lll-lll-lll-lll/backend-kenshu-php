016308c9814c3f9844deaaf9c0c0d0f1af7cdb17
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-06-03 15:03:30 +0900

    [feat] タグの複数インサートSQL実装

--- app/src/Repository/CreateArticleTagRepository.php
+++ app/src/Repository/CreateArticleTagRepository.php
@@ -7,16 +7,26 @@ use PDO;
 
 class CreateArticleTagRepository
 {
+    /**
+     * @param PDO $pdo
+     * @param int $article_id
+     * @param array<int> $tags
+     */
     public function execute(PDO $pdo, int $article_id, array $tags): void
     {
-        $sql = '
-            INSERT INTO "article_tag" (article_id, tag_id)
-            VALUES (:article_id, :tag_id)
-        ';
+        $sql = 'INSERT INTO "article_tag" (article_id, tag_id) VALUES ';
+        $values = [];
+        $params = [];
+
+        foreach ($tags as $index => $tag_id) {
+            $values[] = "(:article_id_$index, :tag_id_$index)";
+            $params[":article_id_$index"] = $article_id;
+            $params[":tag_id_$index"] = $tag_id;
+        }
+
+        $sql .= implode(", ", $values);
+        echo $sql;
         $stmt = $pdo->prepare($sql);
-        $stmt->execute([
-            ':article_id' => $article_id,
-            ':tag_id' => $tag_id,
-        ]);
+        $stmt->execute($params);
     }
 }
