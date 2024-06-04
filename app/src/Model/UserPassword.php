78755c69fefa048db22e39e5c86927d65f3793cb
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-06-04 17:56:23 +0900

    [prfix] delete raw password property

--- app/src/Model/UserPassword.php
+++ app/src/Model/UserPassword.php
@@ -8,24 +8,17 @@ use InvalidArgumentException;
 class UserPassword
 {
     private string $hashPassword;
-    private string $rawPassword;
 
     public function __construct(string $password)
     {
         if (empty($password)) {
             throw new InvalidArgumentException('Password is required');
         }
-        $this->rawPassword = $password;
         $this->hashPassword = password_hash($password, PASSWORD_DEFAULT);
     }
 
     public function getHashPassword(): string
     {
         return $this->hashPassword;
-    }
-
-    public function getRawPassword(): string
-    {
-        return $this->rawPassword;
     }
 }
