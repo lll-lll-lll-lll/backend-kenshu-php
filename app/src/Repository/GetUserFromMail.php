268ed059dd084dcffec7bc010fd3aa36281cb149
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-06-04 16:54:53 +0900

    [fix]　updated_at追加

--- app/src/Repository/GetUserFromMail.php
+++ app/src/Repository/GetUserFromMail.php
@@ -15,13 +15,13 @@ class GetUserFromMail
 
     public function execute(PDO $pdo, string $mail): User
     {
-        $stmt = $pdo->prepare('SELECT id, name, mail, password, profile_url, created_at  FROM "user" WHERE mail = :mail');
+        $stmt = $pdo->prepare('SELECT id, name, mail, password, profile_url, created_at, updated_at  FROM "user" WHERE mail = :mail');
         $stmt->bindParam(':mail', $mail, PDO::PARAM_STR);
         $stmt->execute();
         if ($stmt->rowCount() === 0) {
             throw new InvalidArgumentException('user is not found');
         }
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        return new User($result['id'], $result['name'], $result['mail'], $result['password'], $result['profile_url'], $result['created_at']);
+        return new User(id: $result['id'], name: $result['name'], mail: $result['mail'], password: $result['password'], profileUrl: $result['profile_url'], createdAt: $result['created_at'], updatedAt: $result['updated_at']);
     }
 }
