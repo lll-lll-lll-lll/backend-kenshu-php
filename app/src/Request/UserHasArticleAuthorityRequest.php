51ccb4f03c585e7b81fe2ecc53c6bbf84597b705
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-06-07 13:51:46 +0900

    [prfix] 不要なので削除

--- app/src/Request/UserHasArticleAuthorityRequest.php
+++ app/src/Request/UserHasArticleAuthorityRequest.php
@@ -14,10 +14,8 @@ class UserHasArticleAuthorityRequest
     public function __construct(string $articleId, array $dollSession)
     {
         $sessionUserId = (int)$dollSession[Session::USER_ID_KEY];
-        if (!is_numeric($articleId)) {
-            throw new Exception('記事IDが数値ではありません。');
-        }
         $articleIdInt = (int)$articleId;
+
         if (!isset($articleId) || !isset($sessionUserId)) {
             throw new Exception('記事IDまたはユーザーIDが設定されていません。');
         }
