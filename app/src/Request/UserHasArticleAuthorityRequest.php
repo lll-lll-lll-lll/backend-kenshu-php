248034dd109d0652e37b4e389dfc8ee62281e6c8
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-06-10 15:31:45 +0900

    [feat] 権限チェック層

--- app/src/Request/UserHasArticleAuthorityRequest.php
+++ app/src/Request/UserHasArticleAuthorityRequest.php
@@ -11,15 +11,14 @@ class UserHasArticleAuthorityRequest
     public int $articleId;
     public int $sessionUserId;
 
-    public function __construct(string $articleId, array $dollSession)
+    public function __construct(int $articleId, array $dollSession)
     {
         $sessionUserId = (int)$dollSession[Session::USER_ID_KEY];
-        $articleIdInt = (int)$articleId;
 
         if ($articleId < 1 || $sessionUserId < 1) {
             throw new Exception('記事IDまたはユーザーIDが正しくありません');
         }
-        $this->articleId = $articleIdInt;
+        $this->articleId = $articleId;
         $this->sessionUserId = $sessionUserId;
     }
 }
