1ac04184484e2775f1243e3799ac5d1f61abb8a1
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-06-13 17:31:12 +0900

    [feat]　一回のSQLで記事取得に変更

--- app/src/Repository/Article/GetArticleRepository.php
+++ app/src/Repository/Article/GetArticleRepository.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 namespace App\Repository\Article;
 
 use App\Model\Article;
+use App\Model\ArticleImage;
+use App\Model\Tag;
+use App\Model\User;
 use Exception;
 use PDO;
 
@@ -15,14 +18,54 @@ class GetArticleRepository
     public function execute(PDO $pdo, int $id): Article
     {
         try {
-            $stmt = $pdo->prepare('SELECT id, title, contents, created_at, user_id FROM article WHERE id = :id');
+            $stmt = $pdo->prepare('
+                SELECT 
+                    a.id AS article_id,
+                    a.title,
+                    a.contents,
+                    a.created_at AS article_created_at,
+                    u.id AS user_id,
+                    u.name AS user_name,
+                    u.mail AS user_mail,
+                    u.profile_url AS user_profile_url,
+                    ai.thumbnail_image_path,
+                    ai.sub_image_path,
+                    t.id AS tag_id,
+                    t.name AS tag_name
+                FROM 
+                    article a
+                JOIN 
+                    "user" u ON a.user_id = u.id
+                LEFT JOIN 
+                    article_image ai ON a.id = ai.article_id
+                LEFT JOIN 
+                    article_tag at ON a.id = at.article_id
+                LEFT JOIN 
+                    tag t ON at.tag_id = t.id
+                    WHERE a.id = :id;');
             $stmt->bindValue(':id', $id, PDO::PARAM_INT);
             $stmt->execute();
             $result = $stmt->fetch(PDO::FETCH_ASSOC);
             if ($result === false) {
                 throw new Exception("Article with ID $id not found.");
             }
-            return new Article($result['id'], $result['title'], $result['contents'], $result['created_at'], $result['user_id']);
+
+            $user = new User(
+                id: (int)$result['user_id'],
+                name: $result['user_name'],
+                mail: $result['user_mail'],
+                profileUrl: $result['user_profile_url'],
+            );
+            $tag = new Tag(
+                id: (int)$result['tag_id'],
+                name: $result['tag_name']
+            );
+
+            $articleImage = new ArticleImage(
+                $result['thumbnail_image_path'],
+                $result['sub_image_path']
+            );
+            return new Article(id: $result['article_id'], title: $result['title'], contents: $result['contents'], created_at: $result['article_created_at'], user: $user, tags: (array)$tag, articleImages: (array)$articleImage);
         } catch (Exception $e) {
             throw new Exception($e->getMessage());
         }
